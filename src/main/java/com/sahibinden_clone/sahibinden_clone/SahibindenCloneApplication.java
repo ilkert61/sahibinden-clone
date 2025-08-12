ed839649e3f2f3217599d9de658e4dc16a353fd9
ilkert61 <ilkerturt61@gmail.com>
2025-08-12 10:11:12 +0300

    başlangıç

---
+++ src/main/java/com/sahibinden_clone/sahibinden_clone/SahibindenCloneApplication.java
@@ -0,0 +1,13 @@
+package com.sahibinden_clone.sahibinden_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SahibindenCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SahibindenCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sahibinden_clone/sahibinden_clone/SahibindenCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahibinden_clone.sahibinden_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahibindenCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
